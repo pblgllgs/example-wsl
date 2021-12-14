25f2bcd55ccc9bc76f9d8801709ae41736ccd2a3
pblgllgs <pbl.gllgs@gmail.com>
2021-12-14 14:50:57 -0300

    updates

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const { response } = require('express')
 const express = require('express')
 
+const mysql = require('mysql2/promise');
+
 const app = express()
 
-app.get('/',(req, res) => {
-    res.send(`<h1>Hello! Welcome!!</h1>`)
-})
+let db
 
-app.listen(3000)
+const go = async () => {
+     db = await mysql.createConnection({
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: 'example',
+        database: 'pets'
+    });
+    app.listen(3000)
+    console.log('corriendo en puerto 3000')
+    console.log('DB online')
+}
+
+go();
+
+app.get('/', async (req, res) => {
+    const [users] = await db.execute('SELECT * FROM users')
+    res.send(`
+    <h1>Pets</h1>
+    <ul>${users.map(animal => 
+        `<li>${animal.name}</li>`).join('')}
+    </ul>
+    `)
+})
